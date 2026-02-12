9605c5444bb38db964c7d495a6187742ea1904c1
ud123df <147704288+ud123df@users.noreply.github.com>
2026-02-12 14:58:09 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -17,7 +17,7 @@ user_input = st.text_input("🔞Enter the YT video")
 user_input = st.text_input("Enter YouTube URL")
 transcript_list=None
 if user_input:
-    try:
+    # try:
         user_inputs = user_input.split('=')[1]
         st.write(user_inputs)
         transcript_list = ytt_api.fetch(user_inputs)
@@ -25,100 +25,101 @@ if user_input:
 
         yt = [i['text'] for i in transcript_list]
 
-    except Exception:
-        st.error("Transcript unavailable or blocked for this video.")
+    # except Exception:
+    #     st.error("Transcript unavailable or blocked for this video.")
 
 ##############################
-yt=[]
-for i in transcript_list:
-    o=str(i)
-    o=o.replace("FetchedTranscriptSnippet","")
-    yt.append(o)
-    # yt.append(o)
-
-#####################################
-
-sts=""
-for i in range(len(yt)):
-    u=yt[0].replace("(", "")
-    u=u.replace(")", "").split(",")
-    u = yt[i].replace("(", "")
-    u=u.replace(")", "")
-    g=u.find(", start")
-    sts= sts+ u[5:g]
-
-######################################
-
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-chunks = splitter.create_documents([sts])
-
-######################################
-
-embeddings = HuggingFaceBgeEmbeddings()
-vector_store = FAISS.from_documents(chunks, embeddings)
-
-
-##################################
-
-retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
-
-####################################
-
-from langchain_groq import ChatGroq
-
-##################################
-
-llm = ChatGroq(
-    model = "meta-llama/llama-4-maverick-17b-128e-instruct", 
-    temperature=0.1
-)
-
-
-###################################
-
-prompt = PromptTemplate(
-    template= """
-      You are a helpful assistant.
-      Answer ONLY from the provided transcript context.
-      If the context is insufficient, just say you don't know.
-      
-      {context}
-      Question: {question}
-    """,
-    input_variables = ['context', 'question']
-)
-
-
-#########################################################
-
-
-question          = st.text_input("Enter the Question 😵 : ")
-retrieved_docs    = retriever.invoke(question)
-
-
-##########################################
-
-
-context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
-# context_text
-
-
-###########################################
-
-final_prompt = prompt.invoke({"context": context_text, "question": question})
-
-###########################################
-
-answer = llm.invoke(final_prompt)
-st.success(answer.content)
-
-# header = st.header("hello worlds")
-
-
-
-
-
-
-
-
-
+        yt=[]
+        for i in transcript_list:
+            o=str(i)
+            o=o.replace("FetchedTranscriptSnippet","")
+            yt.append(o)
+            # yt.append(o)
+        
+        #####################################
+        
+        sts=""
+        for i in range(len(yt)):
+            u=yt[0].replace("(", "")
+            u=u.replace(")", "").split(",")
+            u = yt[i].replace("(", "")
+            u=u.replace(")", "")
+            g=u.find(", start")
+            sts= sts+ u[5:g]
+        
+        ######################################
+        
+        splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        chunks = splitter.create_documents([sts])
+        
+        ######################################
+        
+        embeddings = HuggingFaceBgeEmbeddings()
+        vector_store = FAISS.from_documents(chunks, embeddings)
+        
+        
+        ##################################
+        
+        retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
+        
+        ####################################
+        
+        from langchain_groq import ChatGroq
+        
+        ##################################
+        
+        llm = ChatGroq(
+            model = "meta-llama/llama-4-maverick-17b-128e-instruct", 
+            temperature=0.1
+        )
+        
+        
+        ###################################
+        
+        prompt = PromptTemplate(
+            template= """
+              You are a helpful assistant.
+              Answer ONLY from the provided transcript context.
+              If the context is insufficient, just say you don't know.
+              
+              {context}
+              Question: {question}
+            """,
+            input_variables = ['context', 'question']
+        )
+        
+        
+        #########################################################
+        
+        
+        question          = st.text_input("Enter the Question 😵 : ")
+        retrieved_docs    = retriever.invoke(question)
+        
+        
+        ##########################################
+        
+        
+        context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+        # context_text
+        
+        
+        ###########################################
+        
+        final_prompt = prompt.invoke({"context": context_text, "question": question})
+        
+        ###########################################
+        
+        answer = llm.invoke(final_prompt)
+        st.success(answer.content)
+        
+        # header = st.header("hello worlds")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
